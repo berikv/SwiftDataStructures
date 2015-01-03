134883369c0789818208338e6531a863ddfc8533
Berik Visschers <berik@visschers.nu>
2015-01-03 23:11:39 +0100

    Confirm to CollectionType, implement dictionary and array interfaces

--- SwiftDataStructures/OrderedDictionary.swift
+++ SwiftDataStructures/OrderedDictionary.swift
@@ -1,62 +1,185 @@
 //
 //  OrderedDictionary.swift
-//  SwiftDataStructures
 //
-//  Created by Tim Ekl on 6/2/14.
-//  Copyright (c) 2014 Tim Ekl. All rights reserved.
+//  Created by Berik Visschers,  on 2015-01-03.
+//  Copyright (c) 2015 Berik Visschers. All rights reserved.
 //
 
-import Foundation
-
-struct OrderedDictionary<Tk: Hashable, Tv> {
-    var keys: Array<Tk> = []
-    var values: Dictionary<Tk,Tv> = [:]
+struct OrderedDictionary<Key: Hashable, Value>: CollectionType, DictionaryLiteralConvertible {
+    typealias Index = Int
+    typealias Element = (key: Key, value: Value)
     
-    var count: Int {
-    assert(keys.count == values.count, "Keys and values array out of sync")
-    return self.keys.count;
+    private var _keys = [Key]()
+    var keys: [Key] { return _keys }
+
+    private var _values = [Value]()
+    var values: [Value] { return _values }
+    
+    var startIndex: Index { return 0 }
+    var endIndex: Index { return self.keys.endIndex }
+    
+    var count: Int { return self.keys.count }
+    var isEmpty: Bool { return self.keys.isEmpty }
+    
+    var first: Element? {
+        if let key = keys.first {
+            if let value = values.first {
+                return (key, value)
+            }
+        }
+        return nil
     }
     
-    // Explicitly define an empty initializer to prevent the default memberwise initializer from being generated
+    var last: Element? {
+        if let key = keys.last {
+            if let value = values.last {
+                return (key, value)
+            }
+        }
+        return nil
+    }
+
+    func indexForKey(key: Key) -> Index? { return find(keys, key) }
+
     init() {}
+    init(dictionaryLiteral elements: (Key, Value)...) {
+        for element in elements {
+            _keys.append(element.0)
+            _values.append(element.1)
+        }
+    }
     
-    subscript(index: Int) -> Tv? {
+    func generate() -> GeneratorOf<Element> {
+        var keysGenerator = keys.generate()
+        var valuesGenerator = values.generate()
+        return GeneratorOf {
+            if let key = keysGenerator.next() {
+                if let value = valuesGenerator.next() {
+                    return (key, value)
+                }
+            }
+            return nil
+        }
+    }
+    
+    mutating func append(#key: Key, value: Value) {
+        _keys.append(key)
+        _values.append(value)
+    }
+    
+    mutating func removeLast() -> Element {
+        let key = _keys.removeLast()
+        let value = _values.removeLast()
+        return (key, value)
+    }
+    
+    mutating func insert(newElement: Element, atIndex index: Int) {
+        let (key, value) = newElement
+        _keys.insert(key, atIndex: index)
+        _values.insert(value, atIndex: index)
+    }
+    
+    mutating func updateValue(value: Value, forKey key: Key) -> Value? {
+        if let position = find(keys, key) {
+            let result = values[position]
+            _values[position] = value
+            return result
+        } else {
+            _keys.append(key)
+            _values.append(value)
+            return nil
+        }
+    }
+    
+    mutating func removeAtIndex(index: Index) {
+        _keys.removeAtIndex(index)
+        _values.removeAtIndex(index)
+    }
+    
+    mutating func removeValueForKey(key: Key) -> Value? {
+        if let position = find(keys, key) {
+            let result = values[position]
+            _keys.removeAtIndex(position)
+            _values.removeAtIndex(position)
+            return result
+        } else {
+            return nil
+        }
+    }
+    
+    mutating func removeAll(keepCapacity: Bool = false) {
+        _keys.removeAll(keepCapacity: keepCapacity)
+        _values.removeAll(keepCapacity: keepCapacity)
+    }
+    
+    subscript(position: Index) -> Element {
         get {
-            let key = self.keys[index]
-            return self.values[key]
+            return (keys[position], values[position])
         }
         set(newValue) {
-            let key = self.keys[index]
-            if newValue {
-                self.values[key] = newValue
+            let (key, value) = newValue
+            if let position = find(keys, key) {
+                _values[position] = value
             } else {
-                self.values.removeValueForKey(key)
-                self.keys.removeAtIndex(index)
+                _keys.append(key)
+                _values.append(value)
             }
         }
     }
     
-    subscript(key: Tk) -> Tv? {
+    subscript(key: Key) -> Value? {
         get {
-            return self.values[key]
+            if let index = find(self.keys, key) {
+                return values[index]
+            }
+            return nil
         }
         set(newValue) {
-            if newValue == nil {
-                self.values.removeValueForKey(key)
-                self.keys.filter {$0 != key}
-            }
-            
-            let oldValue = self.values.updateValue(newValue!, forKey: key)
-            if oldValue == nil {
-                self.keys.append(key)
+            if let value = newValue {
+                if let index = find(self.keys, key) {
+                    _values[index] = value
+                } else {
+                    _keys.append(key)
+                    _values.append(value)
+                }
+            } else {
+                if let index = find(self.keys, key) {
+                    _keys.removeAtIndex(index)
+                    _values.removeAtIndex(index)
+                }
             }
         }
     }
     
+    mutating func sort(isOrderedBefore: (Element, Element) -> Bool) {
+        abort() // Exercise for the reader
+    }
+    
+    func map<U>(transform: (Element) -> U) -> [U] {
+        abort() // Exercise for the reader
+    }
+    
+    func filter(includeElement: (Element) -> Bool) -> [Element] {
+        abort() // Exercise for the reader
+    }
+    
+    func reverse() -> OrderedDictionary {
+        var result = OrderedDictionary<Key, Value>()
+        result._keys = keys.reverse()
+        result._values = values.reverse()
+        return result
+    }
+}
+
+extension OrderedDictionary: Printable, DebugPrintable {
+    var debugDescription: String {
+        return description
+    }
+
     var description: String {
         var result = "{\n"
-        for i in 0..self.count {
-            result += "[\(i)]: \(self.keys[i]) => \(self[i])\n"
+        for (index, (key, value)) in enumerate(self) {
+            result += "[\(index)]: \(key) => \(value)\n"
         }
         result += "}"
         return result
