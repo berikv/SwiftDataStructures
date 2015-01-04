857b1c6b74e63dace9756a67ec62e833752c1f57
Berik Visschers <berik@visschers.nu>
2015-01-04 22:27:37 +0100

    Dictionary like behaviour fixed
    Operations implemented: map, sort, filter, ==, +, +=

--- SwiftDataStructures/OrderedDictionary.swift
+++ SwiftDataStructures/OrderedDictionary.swift
@@ -1,7 +1,7 @@
 //
 //  OrderedDictionary.swift
 //
-//  Created by Berik Visschers,  on 2015-01-03.
+//  Created by Berik Visschers, on 2015-01-03.
 //  Copyright (c) 2015 Berik Visschers. All rights reserved.
 //
 
@@ -12,8 +12,10 @@ struct OrderedDictionary<Key: Hashable, Value>: CollectionType, DictionaryLitera
     private var _keys = [Key]()
     var keys: [Key] { return _keys }
 
-    private var _values = [Value]()
-    var values: [Value] { return _values }
+    private var _storage = [Key: Value]()
+    var values: [Value] {
+        return keys.map { self._storage[$0]! }
+    }
     
     var startIndex: Index { return 0 }
     var endIndex: Index { return self.keys.endIndex }
@@ -42,10 +44,16 @@ struct OrderedDictionary<Key: Hashable, Value>: CollectionType, DictionaryLitera
     func indexForKey(key: Key) -> Index? { return find(keys, key) }
 
     init() {}
+    
+    init(_ orderedDictionary: OrderedDictionary<Key, Value>) {
+        self._keys = orderedDictionary._keys
+        self._storage = orderedDictionary._storage
+    }
+    
     init(dictionaryLiteral elements: (Key, Value)...) {
         for element in elements {
             _keys.append(element.0)
-            _values.append(element.1)
+            _storage[element.0] = element.1
         }
     }
     
@@ -63,110 +71,114 @@ struct OrderedDictionary<Key: Hashable, Value>: CollectionType, DictionaryLitera
     }
     
     mutating func append(#key: Key, value: Value) {
-        _keys.append(key)
-        _values.append(value)
+        if _storage[key] == nil {
+            _keys.append(key)
+        }
+        _storage[key] = value
     }
-    
+        
     mutating func removeLast() -> Element {
         let key = _keys.removeLast()
-        let value = _values.removeLast()
+        let value = _storage.removeValueForKey(key)!
         return (key, value)
     }
     
-    mutating func insert(newElement: Element, atIndex index: Int) {
+    mutating func insert(newElement: Element, atIndex index: Index) {
         let (key, value) = newElement
-        _keys.insert(key, atIndex: index)
-        _values.insert(value, atIndex: index)
+        if _storage[key] == nil {
+            _keys.insert(key, atIndex: index)
+        }
+        _storage[key] = value
     }
     
     mutating func updateValue(value: Value, forKey key: Key) -> Value? {
-        if let position = find(keys, key) {
-            let result = values[position]
-            _values[position] = value
-            return result
-        } else {
+        if _storage[key] == nil {
             _keys.append(key)
-            _values.append(value)
-            return nil
         }
+        
+        return _storage.updateValue(value, forKey: key)
     }
     
     mutating func removeAtIndex(index: Index) {
-        _keys.removeAtIndex(index)
-        _values.removeAtIndex(index)
+        let key = _keys.removeAtIndex(index)
+        _storage.removeValueForKey(key)
     }
     
     mutating func removeValueForKey(key: Key) -> Value? {
         if let position = find(keys, key) {
-            let result = values[position]
             _keys.removeAtIndex(position)
-            _values.removeAtIndex(position)
-            return result
-        } else {
-            return nil
         }
+        return _storage.removeValueForKey(key)
     }
     
     mutating func removeAll(keepCapacity: Bool = false) {
         _keys.removeAll(keepCapacity: keepCapacity)
-        _values.removeAll(keepCapacity: keepCapacity)
+        _storage.removeAll(keepCapacity: keepCapacity)
     }
     
     subscript(position: Index) -> Element {
         get {
-            return (keys[position], values[position])
+            let key = keys[position]
+            return (key, _storage[key]!)
         }
         set(newValue) {
             let (key, value) = newValue
-            if let position = find(keys, key) {
-                _values[position] = value
-            } else {
+            if find(keys, key) == nil {
                 _keys.append(key)
-                _values.append(value)
             }
+            _storage[key] = value
         }
     }
     
     subscript(key: Key) -> Value? {
         get {
-            if let index = find(self.keys, key) {
-                return values[index]
-            }
-            return nil
+            return _storage[key]
         }
         set(newValue) {
             if let value = newValue {
-                if let index = find(self.keys, key) {
-                    _values[index] = value
-                } else {
+                if _storage[key] == nil {
                     _keys.append(key)
-                    _values.append(value)
                 }
+                _storage[key] = value
             } else {
                 if let index = find(self.keys, key) {
                     _keys.removeAtIndex(index)
-                    _values.removeAtIndex(index)
+                    _storage.removeValueForKey(key)
                 }
             }
         }
     }
     
     mutating func sort(isOrderedBefore: (Element, Element) -> Bool) {
-        abort() // Exercise for the reader
+        return _keys.sort {
+            let lhs = ($0, self._storage[$0]!)
+            let rhs = ($1, self._storage[$1]!)
+            return isOrderedBefore(lhs, rhs)
+        }
     }
     
     func map<U>(transform: (Element) -> U) -> [U] {
-        abort() // Exercise for the reader
+        var result = [U]()
+        for element in self {
+            result.append(transform(element))
+        }
+        return result
     }
     
-    func filter(includeElement: (Element) -> Bool) -> [Element] {
-        abort() // Exercise for the reader
+    func filter(includeElement: (Element) -> Bool) -> OrderedDictionary<Key, Value> {
+        var result = OrderedDictionary<Key, Value>()
+        for element in self {
+            if includeElement(element) {
+                result.append(element)
+            }
+        }
+        return result
     }
     
     func reverse() -> OrderedDictionary {
         var result = OrderedDictionary<Key, Value>()
         result._keys = keys.reverse()
-        result._values = values.reverse()
+        result._storage = _storage
         return result
     }
 }
@@ -184,4 +196,20 @@ extension OrderedDictionary: Printable, DebugPrintable {
         result += "}"
         return result
     }
+}
+
+func ==<Key: Equatable, Value: Equatable>(lhs: OrderedDictionary<Key, Value>, rhs: OrderedDictionary<Key, Value>) -> Bool {
+    return lhs.keys == rhs.keys && lhs.values == rhs.values
+}
+
+func +=<Key, Value>(inout lhs: OrderedDictionary<Key, Value>, rhs: OrderedDictionary<Key, Value>) {
+    for element in rhs {
+        lhs.append(key: element.key, value: element.value)
+    }
+}
+
+func +<Key, Value>(lhs: OrderedDictionary<Key, Value>, rhs: OrderedDictionary<Key, Value>) -> OrderedDictionary<Key, Value> {
+    var dict = OrderedDictionary<Key, Value>(lhs)
+    dict += rhs
+    return dict
 }
